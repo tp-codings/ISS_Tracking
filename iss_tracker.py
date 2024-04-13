a3551d2366bb6b52a3870101521e491a0d151840
tp-codings <tobipaul50@gmail.com>
2024-04-13 09:30:32 +0200

    refreshed requirements.txt

--- iss_tracker.py
+++ iss_tracker.py
@@ -2,7 +2,8 @@ import requests
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from geopy.geocoders import Nominatim
-import time
+import keyboard
+import sys
 
 geolocator = Nominatim(user_agent="iss_tracker")
 
@@ -11,11 +12,10 @@ def get_iss_data():
     data = response.json()
     longitude = data['longitude']
     latitude = data['latitude']
-    velocity = data['velocity']  # Geschwindigkeit in km/h
-    altitude = data['altitude']  # Höhe in km
+    velocity = data['velocity']  
+    altitude = data['altitude']  
     return longitude, latitude, velocity, altitude
 
-# Karte erstellen
 plt.figure(figsize=(10, 8))
 m = Basemap(projection='mill',llcrnrlat=-60,urcrnrlat=80,\
             llcrnrlon=-180,urcrnrlon=180,resolution='c')
@@ -24,54 +24,45 @@ m.drawparallels(range(-90, 91, 30), labels=[1,0,0,0])
 m.drawmeridians(range(-180, 181, 45), labels=[0,0,0,1])
 plt.title('Live Position of ISS')
 
-plt.ion()  # Aktiviere interaktives Matplotlib-Backend
+stop = False
+previous_plot = None  
+first_iteration = True 
 
-try:
-    previous_plot = None  # Variable zur Aufbewahrung des vorherigen Plots
-    first_iteration = True  # Variable für die erste Iteration
-    while True:
-        longitude, latitude, velocity, altitude = get_iss_data()
-        print(latitude, longitude)
-        x, y = m(longitude, latitude)
-        
-        # Lösche den vorherigen Plot, falls vorhanden
-        if previous_plot:
-            previous_plot.remove()
-        
-        try:
-            # Ermittle die Länderinformation
-            location = geolocator.reverse((latitude, longitude), timeout=10)
-            country = location.raw['address']['country']
-            print(country)
-            country = country.encode('utf-8').decode('utf-8')
-            print(country)
-        except Exception as e:
-            print("Fehler bei der Länderermittlung:", str(e))
-            country = "Somewhere over the ocean"
-        
-        # Zeichne den aktuellen Plot und füge die Informationen zur Legende hinzu
-        current_plot = m.plot(x, y, 'ro', markersize=10)[0]
-        plt.draw()  # Aktualisiere das bestehende Fenster
-        
-        # Bei der ersten Iteration, initialisiere die Legende und setze den Text
-        if first_iteration:
-            legend = plt.legend([f'ISS Current Position ({country})\n'
-                                 f'Velocity: {velocity:.2f} km/h\n'
-                                 f'Altitude: {altitude:.2f} km'])
-            first_iteration = False
-        else:
-            # Aktualisiere den Text in der Legende
-            legend.get_texts()[0].set_text(f'ISS Current Position ({country})\n'
-                                           f'Velocity: {velocity:.2f} km/h\n'
-                                           f'Altitude: {altitude:.2f} km')
-        
-        plt.pause(1)  # Warte 1 Sekunde, bevor die nächste Position abgerufen wird
-        
-        # Speichere den aktuellen Plot als vorherigen Plot für die nächste Iteration
-        previous_plot = current_plot
+while not stop:
+    longitude, latitude, velocity, altitude = get_iss_data()
+    print(latitude, longitude)
+    x, y = m(longitude, latitude)
+    
+    if previous_plot:
+        previous_plot.remove()
+    
+    try:
+        location = geolocator.reverse((latitude, longitude), timeout=10)
+        country = location.raw['address']['country']
+        print(country)
+        country = country.encode('utf-8').decode('utf-8')
+        print(country)
+    except Exception as e:
+        print("Fehler bei der Länderermittlung:", str(e))
+        country = "Somewhere over the ocean"
+    
+    current_plot = m.plot(x, y, 'ro', markersize=10)[0]
+    plt.draw()  
+    
+    if first_iteration:
+        legend = plt.legend([f'ISS Current Position ({country})\n'
+                                f'Velocity: {velocity:.2f} km/h\n'
+                                f'Altitude: {altitude:.2f} km'])
+        first_iteration = False
+    else:
+        legend.get_texts()[0].set_text(f'ISS Current Position ({country})\n'
+                                        f'Velocity: {velocity:.2f} km/h\n'
+                                        f'Altitude: {altitude:.2f} km')
+    
+    previous_plot = current_plot
+    if keyboard.is_pressed("q"):
+        stop = True
+        sys.exit()
+        break
 
-except KeyboardInterrupt:
-    print("Programm beendet.")
-finally:
-    plt.ioff()  # Deaktiviere interaktives Matplotlib-Backend
-    plt.show()  # Halte das Fenster geöffnet, nachdem die Schleife beendet wurde
+    plt.pause(1)  
--- sattelite_tracker.py
+++ sattelite_tracker.py
@@ -2,7 +2,8 @@ import requests
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from geopy.geocoders import Nominatim
-import time
+import sys
+import keyboard
 
 def get_satellite_data(norad_id):
     print(obs_latitude)
@@ -18,7 +19,6 @@ def get_satellite_data(norad_id):
         return latitude, longitude, altitude
     return None, None, None, None
 
-# Karte erstellen
 plt.figure(figsize=(10, 8))
 m = Basemap(projection='mill',llcrnrlat=-60,urcrnrlat=80,\
             llcrnrlon=-180,urcrnrlon=180,resolution='c')
@@ -29,64 +29,58 @@ plt.title('Live Satellite Positions')
 
 geolocator = Nominatim(user_agent="satellite_tracker")
 
-plt.ion()  # Aktiviere interaktives Matplotlib-Backend
 
 obs_longitude = 6.98165
 obs_latitude = 49.2354
 
-try:
-    satellite_data = [
-        {'norad_id': 25544, 'color': 'red', 'name': 'ISS'},
-        {'norad_id': 20580, 'color': 'blue', 'name': 'Hubble'},
-        {'norad_id': 39084, 'color': 'green', 'name': 'Landsat 8'},
-        {'norad_id': 39460, 'color': 'purple', 'name': 'Tiangong-2'},
-        {'norad_id': 26407, 'color': 'pink', 'name': 'Aqua'},
-        {'norad_id': 25994, 'color': 'blue', 'name': 'GOES-12'},
-        {'norad_id': 43196, 'color': 'red', 'name': 'GSAT-11'},
-        {'norad_id': 37849, 'color': 'purple', 'name': 'WorldView-2'}
-    ]
-    
-    previous_plots = [None] * len(satellite_data)
-    
-    legend_texts = {}    
-    while True:
-        for i, satellite in enumerate(satellite_data):
-            norad_id = satellite['norad_id']
-            latitude, longitude, altitude = get_satellite_data(norad_id)
-            
-            if latitude is None or longitude is None:
-                continue
-            
-            print(f"Satellite {norad_id}: {latitude}, {longitude}")
-            x, y = m(longitude, latitude)
-            
-            if previous_plots[i]:
-                previous_plots[i].remove()
-            
-            # Zeichne den aktuellen Plot und aktualisiere die Legende
-            current_plot = m.plot(x, y, 'o', markersize=10, color=satellite['color'])[0]
-            plt.draw()  # Aktualisiere das bestehende Fenster
-            
-            # Ermittle die Länderinformation
-            location = geolocator.reverse((latitude, longitude))
-            if location is None:
-                country = "Unbekannt"
-            else:
-                country = location.raw.get('address', {}).get('country', "Unbekannt")
+
+satellite_data = [
+    {'norad_id': 25544, 'color': 'red', 'name': 'ISS'},
+    {'norad_id': 20580, 'color': 'blue', 'name': 'Hubble'},
+    {'norad_id': 39084, 'color': 'green', 'name': 'Landsat 8'},
+    {'norad_id': 39460, 'color': 'purple', 'name': 'Tiangong-2'},
+    {'norad_id': 26407, 'color': 'pink', 'name': 'Aqua'},
+    {'norad_id': 25994, 'color': 'blue', 'name': 'GOES-12'},
+    {'norad_id': 43196, 'color': 'red', 'name': 'GSAT-11'},
+    {'norad_id': 37849, 'color': 'purple', 'name': 'WorldView-2'}
+]
+
+previous_plots = [None] * len(satellite_data)
+
+legend_texts = {}    
+while True:
+    for i, satellite in enumerate(satellite_data):
+        norad_id = satellite['norad_id']
+        latitude, longitude, altitude = get_satellite_data(norad_id)
+        
+        if latitude is None or longitude is None:
+            continue
+        
+        print(f"Satellite {norad_id}: {latitude}, {longitude}")
+        x, y = m(longitude, latitude)
+        
+        if previous_plots[i]:
+            previous_plots[i].remove()
+        
+        current_plot = m.plot(x, y, 'o', markersize=10, color=satellite['color'])[0]
+        plt.draw()  
+        
+        location = geolocator.reverse((latitude, longitude))
+        if location is None:
+            country = "Unbekannt"
+        else:
+            country = location.raw.get('address', {}).get('country', "Unbekannt")
 
 
-            if i in legend_texts:
-                legend_texts[i].remove()
+        if i in legend_texts:
+            legend_texts[i].remove()
 
-            legend_texts[i] = plt.text(x+2500000, y, f'{satellite["name"]}\nLat: {latitude:.2f}\nLon: {longitude:.2f}',
-                                        color=satellite['color'], fontsize=10, ha='center', va='center')            
-            # Speichere den aktuellen Plot als vorherigen Plot für die nächste Iteration
-            previous_plots[i] = current_plot
-        plt.pause(1)  # Warte 1 Sekunde, bevor die nächste Position abgerufen wird
+        legend_texts[i] = plt.text(x+2500000, y, f'{satellite["name"]}\nLat: {latitude:.2f}\nLon: {longitude:.2f}',
+                                    color=satellite['color'], fontsize=10, ha='center', va='center')            
+        previous_plots[i] = current_plot
 
-
-except KeyboardInterrupt:
-    print("Programm beendet.")
-finally:
-    plt.ioff()  # Deaktiviere interaktives Matplotlib-Backend
-    plt.show()  # Halte das Fenster geöffnet, nachdem die Schleife beendet wurde
+        if keyboard.is_pressed("q"):
+            stop = True
+            sys.exit()
+            break
+    plt.pause(1)  
